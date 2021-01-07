f08451b97e1e971b20215dc611c9bc3289343657
Baker Quillin <bakerquil@gmail.com>
2021-01-06 21:45:16 -0500

    service worker file moved and styles.css correction made

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/index.js",
     "/db.js",
-    "/style.css",
+    "/styles.css",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "/manifest.webmanifest"

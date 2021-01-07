3c9b98e23f652d8c6c24549982f17865c08f587b
Baker Quillin <bakerquil@gmail.com>
2021-01-06 20:08:09 -0500

    changed mongoose.connect

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,76 +1,68 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
-const iconSizes = ["192", "512"];
-const iconFiles = iconSizes.map(
-    (size) => `/icons/icon-${size}x${size}.png`
-)
 
 const FILES_TO_CACHE = [
     "/",
+    "/index.html",
+    "/index.js",
+    "/db.js",
     "/style.css",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-    "/manifest.webmanifest",
-    "/db.js",
-    "/index.js"
-  ].concat(iconFiles);
+    "/manifest.webmanifest"
+];
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-// reference taken from lesson 14
-  self.addEventListener("install", function(evt) {
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
-        return cache.addAll(FILES_TO_CACHE);
-      })
+self.addEventListener(`install`, event => {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
     );
-  
-    self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
-    );
-  
-    self.clients.claim();
-  });
-  
-  self.addEventListener("fetch", function(evt) {
-    const {url} = evt.request;
-    if (url.includes("/all") || url.includes("/find")) {
-      evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(evt.request)
-            .then(response => {
-              if (response.status === 200) {
-                cache.put(evt.request, response.clone());
-              }
-  
-              return response;
-            })
-            .catch(err => {
-              return cache.match(evt.request);
-            });
-        }).catch(err => console.log(err))
-      );
-    } else {
-      evt.respondWith(
-        caches.open(CACHE_NAME).then(cache => {
-          return cache.match(evt.request).then(response => {
-            return response || fetch(evt.request);
-          });
+});
+
+
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys().then(keyLists => {
+            return Promise.all(
+                keyLists.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("rmv old", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
         })
-      );
-    }
-  });
-  
+    )
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    if(event.request.url.includes('/api/')) {
+        console.log('[Service Worker] Fetch(data)', event.request.url);
+    
+event.respondWith(
+                caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                .then(response => {
+                    if (response.status === 200){
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(event.request);
+                });
+            })
+            );
+            return;
+        }
+event.respondWith(
+    caches.open(CACHE_NAME).then( cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request);
+      });
+    })
+  );
+});
